268604f72b65b3a8de11f3ecb27163cb99467d9e
mohammed ali <mohammedgarouj7@gmail.com>
2025-07-07 09:22:43 +0100

    learn cpp

---
+++ learn_cpp/syntax.cpp
@@ -0,0 +1,77 @@
+// 1 printf hello world in cpp
+
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     cout << "hello world \n" ;
+//     return 0;
+// }
+
+
+// 2 exemple for use variable 
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main(void)
+// {
+
+//     int year = 20;
+//     char name[] = "mohammed ali";
+
+//     cout << "hello my name is " << name << " im " << year << " old " ;
+// }
+
+
+
+
+
+
+
+
+// 3 exercice mini calcul
+
+// #include <iostream>
+
+// using namespace std;
+
+// int main(void)
+// {
+//     int x = 3;
+//     int y = 3;
+
+//     cout << "x + y = " << endl << x + y ;
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << "Yes, you hear the alert :)\a \a \a";
+//     return 0;
+// }
+
+
+
+
+
+// print datatype
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    int x = 3;
+    char c = 'c';
+    cout << x << c << endl;
+}
