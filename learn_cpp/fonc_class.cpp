14458651d42ba09615d73cc365ec8fcea6b27d3e
mohammed ali <mohammedgarouj7@gmail.com>
2025-07-14 15:24:20 +0100

    object_parametre

--- learn_cpp/fonc_class.cpp
+++ learn_cpp/fonc_class.cpp
@@ -1,3 +1,55 @@
+//exemple 1 
+// #include <iostream>
+
+// using namespace std;
+
+// // يحتوي على 4 متغيرات Book هنا قمنا بتعريف كلاس إسمه
+// class Book {
+
+// public:
+//     string title;
+//     string author;
+//     double price;
+//     int numberOfPages;
+
+//     // تقوم بطباعة كل قيم المتغيرات الموجودة فيه Book عند استدعاءها من أي كائن ننشئه من printInfo هنا قمنا بتعريف دالة إسمها
+//     void printInfo()
+//     {
+//         cout << "Title: " << title << "\n";
+//         cout << "Author: " << author << "\n";
+//         cout << "Price: " << price << "$\n";
+//         cout << "Number of pages: " << numberOfPages << "\n";
+//     }
+
+// };
+
+// // main() هنا قمنا بتعريف الدالة
+// int main()
+// {
+//     // book إسمه Book هنا قمنا بتعريف كائن من
+//     Book book;
+
+//     // book هنا قمنا بإعطاء قيم لمتغيرات الكائن
+//     book.title = "C++ for beginners";
+//     book.author = "Mhamad Harmush";
+//     book.price = 9.99;
+//     book.numberOfPages = 420;
+    
+//     // حتى تقوم بطباعة القيم الموجودة فيه book من الكائن printInfo() هنا قمنا باستدعاء الدالة
+//     book.printInfo();
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
 #include <iostream>
 
 using namespace std;
