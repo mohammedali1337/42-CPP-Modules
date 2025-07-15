8a1e359b6b72209eacd1a2bcb13ae460a4ee5688
mohammedali1337 <mohammedgarouj7@gmail.com>
2025-07-15 22:34:03 +0100

    add ex01

---
+++ intra/ex01/PhoneBook.cpp
@@ -0,0 +1,13 @@
+#include "PhoneBook.hpp"
+
+int main(void)
+{
+    std::string input;
+    while (true)
+    {
+        std::getline(std::cin, input);
+        if (input == "ADD")
+        else if (input == "SEARCH")
+        else if (input == "EXIT")
+    }
+}
---
+++ intra/ex01/PhoneBook.hpp
@@ -0,0 +1,14 @@
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+
+#include <iostream>
+#include <iomanip>
+
+class PhoneBook
+{
+    public:
+        std::string contact;
+};
+
+#endif
