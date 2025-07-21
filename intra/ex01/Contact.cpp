5175287b857d567a772e0eb1047199d84c59052d
mohammed ali <mohammedgarouj7@gmail.com>
2025-07-21 03:36:15 +0100

    ex01

--- intra/ex01/Contact.cpp
+++ intra/ex01/Contact.cpp
@@ -1,3 +1,45 @@
-#include "PhoneBook.hpp"
+#include "Contact.hpp"
 
+void    Contact::setFirstName(std::string fn)
+{
+    this->firstName = fn;
+}
+void    Contact::setlastName(std::string ln)
+{
+    this->lastName = ln;
+}
+void    Contact::setnickname(std::string nn)
+{
+    this->firstName = nn;
+}
+void    Contact::setphoneNumber(std::string ph)
+{
+    this->phoneNumber = ph;
+}
+void    Contact::setdarkestSecret(std::string ds)
+{
+    this->darkestSecret = ds;
+}
+
+
+std::string    getFirstName()
+{
+    return (this->firstName)
+}
+std::string    getlastName()
+{
+    return (this->lastName)
+}
+std::string    getnickname()
+{
+    return (this->nickname)
+}
+std::string    getphoneNumber()
+{
+    return (this->phoneNumber)
+}
+std::string    getdarkestSecret()
+{
+    return (this->darkestSecret)
+}
 
---
+++ intra/ex01/Contact.hpp
@@ -0,0 +1,24 @@
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+
+#include <iostream>
+#include <string>
+
+class   Contact
+{
+    private:
+        std::string firstName;
+        std::string lastName;
+        std::string nickname;
+        std::string phoneNumber;
+        std::string darkestSecret;
+    public:
+        void setFirstName(std::string fn);
+        void setlastName(std::string ln);
+        void setnickname(std::string nn);
+        void setphoneNumber(std::string ph);
+        void setdarkestSecret(std::string ds);
+
+};
+#endif
