da0e56338273df71741d43525efa7009fe0c3cce
mohammed ali <mohammedgarouj7@gmail.com>
2025-09-08 13:56:31 +0100

    ex03

---
+++ intra/cpp04/ex03/AMateria.cpp
@@ -0,0 +1,22 @@
+#include "AMateria.hpp"
+
+AMateria::AMateria(){}
+
+AMateria::AMateria(std::string const & type): type(type){}
+
+AMateria::AMateria(AMateria const & ob): type(ob.type){}
+
+AMateria::~AMateria(){}
+
+std::string const & AMateria::getType() const {return (this->type);}
+
+AMateria* AMateria::clone() const
+{
+    AMateria *ob = new AMateria(type);
+    return ob;
+}
+
+void AMateria::use(ICharacter& target)
+{
+    
+}
